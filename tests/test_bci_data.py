66052f6ebe12cae28e89929bc9fa66e1c355cca8
ekinney-lang <ekinneyL44@gmail.com>
2023-02-06 16:21:20 -0700

    Update test_bci_data.py

--- tests/test_bci_data.py
+++ tests/test_bci_data.py
@@ -1,9 +1,23 @@
 import unittest
 import numpy as np
 
-class TestBCIDataSuite(unittest.TestCase):
+from bci_essentials.bci_data import *
 
-    def setUp():
+class TestEEGDataClass(unittest.TestCase):
+
+    def setUp(self):
+        self.eeg = EEG_data()
+        #set up the conditions
+    
+    def EditSettings_EditsEEGDataSettings(self,user_id='123',nchannels=2,channel_labels=['t1','t2'],fsample=128,max_size=1000)
+        
+
+    def tearDown():
+        #teardown the conditions
+
+
+class TestERPDataClass(unittest.TestCase):
+        def setUp():
 
         #set up the conditions
     
