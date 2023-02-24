b80e1455714de7879d942426f5ef2bcad08fdff6
BCI4Kids <>
2023-02-24 16:47:05 -0700

    Update p300_unity_backend.py

--- examples/p300_unity_backend.py
+++ examples/p300_unity_backend.py
@@ -14,8 +14,8 @@ test_erp = ERP_data()
 test_erp.classifier = erp_rg_classifier() # you can add a subset here
 
 # Set some settings
-test_erp.classifier.set_p300_clf_settings(n_splits=5, lico_expansion_factor=1, oversample_ratio=0, undersample_ratio=0, covariance_estimator='oas')
-
+#test_erp.classifier.set_p300_clf_settings(n_splits=5, lico_expansion_factor=1, oversample_ratio=0, undersample_ratio=0, covariance_estimator='oas')
+test_erp.classifier.set_p300_clf_settings(n_splits=5, oversample_ratio=0, undersample_ratio=0, covariance_estimator='oas')
 # initial_subset = []
 # test_erp.classifier.setup_channel_selection(method = "SBFS", metric="accuracy", initial_channels = initial_subset,      # wrapper setup
 #                             max_time= 999, min_channels=2, max_channels=8, performance_delta=0,                        # stopping criterion
@@ -23,7 +23,7 @@ test_erp.classifier.set_p300_clf_settings(n_splits=5, lico_expansion_factor=1, o
 
 # Connect the streams
 test_erp.stream_online_eeg_data() # you can also add a subset here
-
+#test_erp.stream_online_eeg_data(subset = ['P3', 'C3', 'F3', 'Fz', 'F4', 'C4', 'P4', 'Pz', 'Fp1', 'Fp2', 'T3', 'T5', 'O1', 'O2', 'T6', 'T4']) # you can also add a subset here
 # Run main
 # test_erp.main(online=True, training=True, pp_low=0.1, pp_high=15, pp_order=5, plot_erp=False, window_start=0.0, window_end=0.6)
-test_erp.main(online=True, training=True, pp_low=0.1, pp_high=10, pp_order=5, plot_erp=False, window_start=0.0, window_end=0.8, print_markers=False, print_training=False, print_fit=False, print_performance=True, print_predict=False)
+test_erp.main(online=True, training=True, pp_low=0.1, pp_high=10, pp_order=5, plot_erp=False, window_start=0.0, window_end=0.8, print_markers=True, print_training=True, print_fit=True, print_performance=True, print_predict=True,max_decisions=100)
